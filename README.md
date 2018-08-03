4fbd7f85c474aaaf1a9db799c2b027574761d22a
vec <vec@torzo.club>
2018-08-03 17:48:41 +0800

    使用 webpack 构建页面前端代码

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Pache 3
 ## 安装
 
 ```
-npm install pache-blog --g
+yarn global add pache-blog
 ```
 
 ## 要求
@@ -22,9 +22,22 @@ pache config ./config_name.suc
 pache run ./config_name.suc
 ```
 
+## 开发
+
+开发需要安装 `devDependencies` 引用。并且需要启动 `webpack`:
+
+```bash
+# 打包前端代码到 app/page-dist/
+yarn page-generate
+
+# watch 模式
+yarn page-watch
+```
+
 ## 关于配置文件
 
-因为采用了 suc，故可能需要查看以下相关教程。目录中的 config.suc 当中有相关设置项的说明。
+因为采用了 suc，故可能需要查看以下相关教程。目录中的 config.suc 当中有相关设置项的说明。<br>
+也可以使用 `pache config <config_name>` 根据操作向导创建配置文件
 
 ## License
 
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const path = require('path')
+
+const cssRule = {
+  test: /\.css$/,
+  use: [ 'style-loader', 'css-loader' ]
+}
+const stylusRule = {
+  test: /\.styl$/,
+  use: [ 'style-loader', 'css-loader', 'stylus-loader' ],
+}
+const lessRule = {
+  test: /\.less$/,
+  use: [ 'style-loader', 'css-loader', 'less-loader' ],
+}
+
+const entry = {
+  article: path.resolve(__dirname, 'app/page/article/index.js')
+}
+
+const OUTPUT_PATH = path.resolve(__dirname, 'app/page-dist')
+
+module.exports = [{
+  // 非 polyfill
+  mode: 'development',
+
+  entry,
+
+  output: {
+    path: OUTPUT_PATH,
+    filename: '[name]/index.js',
+  },
+
+  module: {
+    rules: [ cssRule, stylusRule, lessRule ],
+  },
+}, {
+  // JS polyfill
+  // 带上了 Promise、async function 等
+  mode: 'development',
+
+  entry,
+
+  output: {
+    path: OUTPUT_PATH,
+    filename: '[name]/index.polyfill.js',
+  },
+
+  module: {
+    rules: [
+      cssRule,
+      stylusRule,
+      lessRule,
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', {
+                modules: false,
+                targets: {
+                  browsers: ['> 1%', 'last 2 versions', 'not ie <= 10']
+                }
+              }],
+              "stage-2"
+            ],
+            plugins: [ 'transform-runtime' ],
+          },
+        },
+      }
+    ],
+  },
+}]
