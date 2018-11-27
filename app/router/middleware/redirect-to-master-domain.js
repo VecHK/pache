426662e019a592c746bb2a609ac3052f7138b24b
vec <vec@torzo.club>
2018-11-28 01:26:34 +0800

    adjust: https 的跳转主域名改成 http/2 的

--- app/router/middleware/redirect-to-master-domain.js
+++ app/router/middleware/redirect-to-master-domain.js
@@ -1,10 +1,10 @@
 module.exports = function (app, envir) {
   // 是否強制跳轉到主域名
-  envir.force_redirect_to_master_domain && app.use(async (ctx, next) => {
+  envir.FORCE_REDIRECT_TO_MASTER_DOMAIN && app.use(async (ctx, next) => {
     console.warn(ctx.hostname, envir.master_domain)
     if (ctx.hostname.trim() !== envir.master_domain.trim()) {
-      let protocol = envir.force_https ? 'https' : ctx.protocol
-      let port = envir.force_https ? envir.https_port : envir.port
+      let protocol = envir.FORCE_H2 ? 'https' : ctx.protocol
+      let port = envir.FORCE_H2 ? envir.H2_PORT : envir.port
       return ctx.redirect(`${protocol}://${envir.master_domain}:${envir.port}${ctx.url}`)
     }
     await next()
