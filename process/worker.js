ad4b35f689cf99a54b9d0bbc53af968638273464
vec <vec@torzo.club>
2018-11-28 01:23:53 +0800

    add: support http/2

--- process/worker.js
+++ process/worker.js
@@ -3,7 +3,7 @@ require('coffeescript/register')
 const root_dir = require('app-root-path').resolve
 
 const fs = require('fs')
-const https = require('https')
+const http2 = require('http2')
 const koa = require('koa')
 const envir = require('../envir')
 const cluster = require('cluster')
@@ -17,25 +17,25 @@ process.on('message', (message) => {
     const http = require('http')
     let app = require(root_dir('./app'))
 
-    if (envir.enable_https) {
+    if (envir.USE_H2) {
       const credentials = {}
       try {
         Object.assign(credentials, {
-          key: fs.readFileSync(envir.private_key, 'utf8'),
-          cert: fs.readFileSync(envir.certificate, 'utf8'),
+          key: fs.readFileSync(envir.PRIVATE_KEY, 'utf8'),
+          cert: fs.readFileSync(envir.CERTIFICATE, 'utf8'),
         })
       } catch (e) {
         console.error('無法讀取 private_key/certificate 文件')
         throw e
       }
 
-      const httpsServer = https.createServer(credentials, app.callback())
-      httpsServer.listen(envir.https_port)
-      httpsServer.on('error', (err) => {
-        console.error('https Server 錯誤', e.message)
+      const h2 = http2.createSecureServer(credentials, app.callback())
+      h2.listen(envir.H2_PORT)
+      h2.on('error', err => {
+        console.error('http/2 Server', err.message)
         throw err
       })
-      httpsServer.on('listening', () => { })
+      h2.on('listening', () => { })
 
       var aliasObj
       if (envir.ALIAS_CONFIG_FILE) {
@@ -49,8 +49,8 @@ process.on('message', (message) => {
         }
       }
 
-      if (envir.force_https) {
-        /* 檢查是否是強制使用 https 的配置，如果是就替換 app 為跳轉到 https 的路由 */
+      if (envir.FORCE_H2) {
+        /* 檢查是否是強制使用 http/2 的配置，如果是就替換 app 為跳轉到 http/2 的路由 */
         app = new koa()
 
         envir.ALIAS_CONFIG_FILE && app.use(async (ctx, next) => {
@@ -65,7 +65,7 @@ process.on('message', (message) => {
           }
         })
 
-        if (envir.force_redirect_to_master_domain) {
+        if (envir.FORCE_REDIRECT_TO_MASTER_DOMAIN) {
           app.use(async (ctx, next) => {
             if (ctx.hostname.trim() !== envir.master_domain.trim()) {
               return ctx.redirect(`https://${envir.master_domain}:${envir.port}${ctx.url}`)
