f35d9107ff752d14b0c92aa4648c6a99136ca30e
vec <vec@torzo.club>
2018-12-03 11:30:55 +0800

    优化测试

--- test/api/category.js
+++ test/api/category.js
@@ -7,30 +7,54 @@ const {
   createAdminAgent
 } = require('../_test_envirment')
 
+const _Category = require('../_category')
+let Category
+
 /* 準備環境 */
 let ag = null
 test.before('准备环境', async t => {
   ag = await createAdminAgent()
+  Category = new _Category(ag.agent, ag.token)
 
   await clearModel()
 })
 
-async function createCategory(obj = {}, expect_status_code = 201) {
-  const defaultValue = {
-    name: '分类',
-    position: 'left'
-  }
-  const new_category = Object.assign({
-    name: '分类',
-    position: 'left'
-  }, obj)
+test.serial('分类列表(空)', async t => {
+  let web = await ag('get', '/api/categories').json(200)
 
-  let web = await ag('post', '/api/category').testJson(new_category, expect_status_code)
-  return web.json
-}
+  let data = web.json
+  t.is(typeof data, 'object')
+  t.true(Array.isArray(data.left))
+  t.true(Array.isArray(data.right))
+
+  t.is(data.left.length, 0)
+  t.is(data.right.length, 0)
+})
+
+test.serial('分类列表', async t => {
+  const left = await Category.create({
+    name: '分类_left',
+    position: 'left'
+  })
+  const right = await Category.create({
+    name: '分类_right',
+    position: 'right'
+  })
+
+  let web = await ag('get', '/api/categories').json(200)
+
+  let data = web.json
+
+  t.is(typeof data, 'object')
+  t.true(Array.isArray(data.left))
+  t.true(Array.isArray(data.right))
+
+  t.truthy(data.left.findBy('_id', left._id))
+  t.truthy(data.right.findBy('_id', right._id))
+})
 
 test('创建分类', async t => {
-  let data = await createCategory({
+  let data = await Category.create({
     name: '分类1',
     position: 'right'
   })
@@ -41,9 +65,9 @@ test('创建分类', async t => {
 })
 
 test('不能创建重名的分类', async t => {
-  await createCategory({ name: '重名' })
+  await Category.create({ name: '重名' })
 
-  const err = await createCategory({ name: '重名' }, 409)
+  const err = await Category.create({ name: '重名' }, 409)
   t.is(typeof err, 'object')
   t.truthy(err)
   t.is(typeof err.message, 'string')
@@ -66,48 +90,9 @@ test('删除分类', async t => {
   await ag('get', `/api/category/${deleted._id}`).json(404)
 })
 
-test.serial('分类列表(空)', async t => {
-  let web = await ag('get', '/api/categories').json(200)
-
-  let data = web.json
-  t.is(typeof data, 'object')
-  t.true(Array.isArray(data.left))
-  t.true(Array.isArray(data.right))
-
-  t.is(data.left.length, 0)
-  t.is(data.right.length, 0)
-})
-
-test.serial('分类列表', async t => {
-  const left = await createCategory({
-    name: '分类_left',
-    position: 'left'
-  })
-  const right = await createCategory({
-    name: '分类_right',
-    position: 'right'
-  })
-
-  let web = await ag('get', '/api/categories').json(200)
-
-  let data = web.json
-
-  t.is(typeof data, 'object')
-  t.true(Array.isArray(data.left))
-  t.true(Array.isArray(data.right))
-
-  t.truthy(data.left.findBy('_id', left._id))
-  t.truthy(data.right.findBy('_id', right._id))
-})
-
-async function getCategory(_id) {
-  const web = await ag('get', `/api/category/${_id}`).json(200)
-  return web.json
-}
-
 test('修改分类', async t => {
   const sourceValue = { name: 'source', color: '#BBBBBB' }
-  const new_category = await createCategory(sourceValue)
+  const new_category = await Category.create(sourceValue)
 
   const web = await ag('patch', `/api/category/${new_category._id}`).testJson({
     name: '新名字',
@@ -120,7 +105,7 @@ test('修改分类', async t => {
   t.is(mod_category.color, '#CCCCCC')
   t.is(mod_category.position, 'right')
 
-  const category = await getCategory(mod_category._id)
+  const category = await Category.get(mod_category._id)
 
   t.is(category.name, '新名字')
   t.is(category.color, '#CCCCCC')
@@ -128,9 +113,9 @@ test('修改分类', async t => {
 })
 
 test('修改分类(重复分类)', async t => {
-  await createCategory({ name: 'duplicate' })
+  await Category.create({ name: 'duplicate' })
 
-  const new_category = await createCategory({ name: 'other_category' })
+  const new_category = await Category.create({ name: 'other_category' })
   const web = await ag('patch', `/api/category/${new_category._id}`).testJson({
     name: 'duplicate'
   }, 409)
